3b2217a8cf1083cb362b264068d785361fe805b1
laloo <lelisahailu21@gmail.com>
2026-08-16 21:41:48 +0300

    feat: Just exploring redux saga

--- frontend/src/features/ideas/ideasSaga.ts
+++ frontend/src/features/ideas/ideasSaga.ts
@@ -1,11 +1,29 @@
+import axios, { AxiosResponse } from "axios";
 import { call, put, takeLatest } from "redux-saga/effects"; // saga effets
+import { Idea } from "./types";
+import { fetchIdeasSuccess } from "./ideasSlice";
 
 const API_URL = "/api/ideas";
+
+// API Helpers
+const fetchIdeasAPI = () =>   axios.get<Idea[]>(API_URL);
+const addIdeaAPI = (newIdea: {title:string, description:string}) => axios.post<Idea>(API_URL, newIdea);
 
 function* workerSagaFunction() {
   yield "demo function";
 }
 
+function*workFetchIdeas():Generator<unknown, void, AxiosResponse<Idea[]>>{
+  try {
+    const response = yield call(fetchIdeasAPI);
+    yield put(fetchIdeasSuccess(response.data))
+  } catch (error) {
+    
+  }
+}
 export function* ideasSaga() {
   // yield takeLatest(payload_type, worker_function);
+  yield takeLatest("ideas/fetchIdeasRequest", workFetchIdeas)
+
+
 }
--- frontend/src/features/ideas/ideasSlice.ts
+++ frontend/src/features/ideas/ideasSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import type { Idea } from "./types";
 
 export type IdeasStatus = "idle" | "loading" | "succeeded" | "failed";
@@ -18,9 +18,40 @@ const initialState: IdeasState = {
 const ideasSlice = createSlice({
   name: "ideas",
   initialState,
-  reducers: {},
+  reducers: {
+    fetchIdeasRequested:(state) =>{
+      state.status = "loading"
+      state.error = null
+    },
+     fetchIdeasSuccess:(state, action:PayloadAction<Idea[]>) => {
+      state.list = action.payload
+      state.status = "succeeded"
+    },
+     fetchIdeasFailure:(state, action:PayloadAction<string>) =>{
+      state.status = "failed"
+      state.error = action.payload
+    },
+
+    // add Idea action
+    addIdeaRequested:(state,  _action: PayloadAction<{title:string, description:string}> ) =>{
+    state.status = "loading"
+    },
+
+    addIdeaSuccess:(state, action:PayloadAction<Idea>) => {
+     state.status = "succeeded"
+     state.list.push(action.payload)
+    },
+    addIdeaFailure:(state, action:PayloadAction<string>) => {
+       state.status  = "failed"
+       state.error = action.payload
+    }
+
+  },
+ 
 });
 
-export const {} = ideasSlice.actions;
+export const {fetchIdeasRequested,fetchIdeasSuccess, fetchIdeasFailure,
+             addIdeaRequested, addIdeaSuccess, addIdeaFailure
+ } = ideasSlice.actions;
 
 export default ideasSlice.reducer;
